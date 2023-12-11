23849c84d949b88ce7a5ff7df45f4baa99a89696
Mikael Winter <54064888+michalwinter@users.noreply.github.com>
2023-12-11 22:59:02 +0100

    fix 3

--- main.py
+++ main.py
@@ -19,26 +19,24 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def cache_logging(train_id, yet, total):
   os.system('cls')
   print(f"Caching train {train_id} - {yet}/{total}", end='\r')
-
-def cache_train(**kwargs):
-  train_ids = kwargs.get("trains_ids", [])
-  trains = {}
-  print(f"Getting routes of {len(train_ids)} trains")
-  make_response(f"Getting routes of {len(train_ids)} trains", 200)
-
-  for train_id in train_ids:
-    cache_logging(train_id, len(trains) + 1, len(train_ids))
-    trains[train_id] = get_plain_train_route(train_id)
-    #sleep(0.2 + (0.6 * random()))
-
-  post(url=mother_url, json=trains)
   
 
 @app.post("/")
 def index():
-  train_ids = request.get_json()
-  Thread(target=cache_train, kwargs={"trains_ids": train_ids}).start()
-  return make_response(f"Getting routes of {len(train_ids)} trains", 200)
+  body = request.get_json()
+  def cache_train(**kwargs):
+    train_ids = kwargs.get("trains_ids", [])
+    trains = {}
+    print(f"Getting routes of {len(train_ids)} trains")
+    make_response(f"Getting routes of {len(train_ids)} trains", 200)
+
+    for train_id in train_ids:
+      cache_logging(train_id, len(trains) + 1, len(train_ids))
+      trains[train_id] = get_plain_train_route(train_id)
+      #sleep(0.2 + (0.6 * random()))
+
+    post(url=mother_url, json=trains)
+  Thread(target=cache_train, kwargs={"trains_ids": body}).start()
+  return make_response(f"Getting routes of {len(body)} trains", 200)
   
 app.run(host='0.0.0.0', port=port)
-app.app_context()
